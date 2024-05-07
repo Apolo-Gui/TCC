63dde184162391fad9faa350d7d23ee836d32482
apologui17 <agluiz@faeterj-petropolis.edu.br>
2024-05-07 15:42:04 -0300

    Eu não hablo nada

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,24 @@ elementosDuvida.forEach(function (duvida) {
     duvida.classList.toggle('ativa')
   })
 })
+
+// Quando o usuário rolar a partir do topo do documento, mostrar ou ocultar o botão
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("myBtn").style.opacity = "1"; // Torna o botão visível
+  } else {
+    document.getElementById("myBtn").style.opacity = "0"; // Torna o botão invisível suavemente
+    setTimeout(function(){
+      document.getElementById("myBtn").style.display = "none"; // Esconde o botão após a transição
+    }, 300);
+  }
+}
+
+// Quando o usuário clicar no botão, rolar para o topo do documento
+function topFunction() {
+  document.body.scrollTop = 0; // Para browsers da Safari
+  document.documentElement.scrollTop = 0; // Para outros browsers
+}
